825e13557710f2e61e7b8091067fd6deca965c5d
asifalam515 <asibulalam515@gmail.com>
2025-03-02 12:22:50 +0600

    factorial test added

--- src/mathUtils.js
+++ src/mathUtils.js
@@ -6,3 +6,21 @@ export const divide = (a, b) => {
   if (b === 0) throw new Error("Divide by zero is not allowed");
   return a / b;
 };
+
+export const factorial = (n) => {
+  if (n < 0) throw new Error("Number must be non negative");
+  return n === 0 ? 1 : n * factorial(n - 1);
+};
+
+export const gcd = (a, b) => {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
+
+export const fibonachi = (n) => {
+  if (n < 0) throw new Error("Number must be non negative");
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+};
--- test/mathUtils.test.js
+++ test/mathUtils.test.js
@@ -1,5 +1,13 @@
 import { expect, test, it, describe } from "vitest";
-import { add, subTrack, multiply, divide } from "../src/mathUtils";
+import {
+  add,
+  subTrack,
+  multiply,
+  divide,
+  fibonachi,
+  gcd,
+  factorial,
+} from "../src/mathUtils";
 
 describe("math utils testing", () => {
   test("add two number", () => {
@@ -14,5 +22,11 @@ describe("math utils testing", () => {
   });
   test("divide one number by another number", () => {
     expect(divide(4, 2)).toBe(2);
+  });
+  test("should throw new error when divided by zero", () => {
+    expect(() => divide(5, 0)).toThrow("Divide by zero is not allowed");
+  });
+  test("should calculate factorial", () => {
+    expect(factorial(5)).toBe(120);
   });
 });
